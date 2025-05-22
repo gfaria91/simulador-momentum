c2687941515d03623d87ca22e0f482c743cba410
Guilherme Faria <gfaria91@gmail.com>
2025-05-22 16:34:38 -0300

    feat: adiciona formulário completo da simulação

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import { SimulacaoCard } from './components/SimulacaoCard'
+import { FormularioSimulacao } from './components/formulario/FormularioSimulacao'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-2xl font-bold text-center text-blue-700 mb-6">
-        Simulador de Financiamento
-      </h1>
-
-      <div className="max-w-xl mx-auto">
-        <SimulacaoCard
-          titulo="BMP Pick Money"
-          entrada={2250}
-          juros={0.33}
-          prazo={180}
-          parcela={1091.02}
-          totalMes={1877.13}
-        />
-
-        <SimulacaoCard
-          titulo="Momentum"
-          entrada={2250}
-          juros={0.0}
-          prazo={24}
-          parcela={6162.41}
-          totalMes={6948.52}
-        />
-      </div>
+    <div className="min-h-screen bg-gray-100 py-10 px-4">
+      <FormularioSimulacao />
     </div>
   )
 }
