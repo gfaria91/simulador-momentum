1deaccc063e3f02feef031d1dd7279a658060579
Guilherme Faria <gfaria91@gmail.com>
2025-05-22 11:54:10 -0300

    fix: aponta index.html como entrada do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(async () => {
-  const react = await import('@vitejs/plugin-react')
-  return {
-    plugins: [react.default()],
-  }
+export default defineConfig({
+  root: '.', // garante que Vite olhe para a raiz do projeto
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html',
+    },
+  },
 })
