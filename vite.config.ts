59a99dc1b9645d57dc36aaf16d6afc538f4ed92e
Guilherme Faria <gfaria91@gmail.com>
2025-05-22 16:12:57 -0300

    fix: move index.html para a raiz do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // garante que Vite olhe para a raiz do projeto
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html',
-    },
-  },
+    outDir: 'dist'
+  }
 })
